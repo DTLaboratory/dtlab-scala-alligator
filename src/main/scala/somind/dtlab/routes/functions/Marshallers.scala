10d40c4169cdd833c91c7b42ed57477805d6f86b
Ed Sweeney <ed@onextent.com>
2020-08-29 16:21:29 -0700

    remove dupe code

--- src/main/scala/somind/dtlab/routes/functions/Marshallers.scala
+++ src/main/scala/somind/dtlab/routes/functions/Marshallers.scala
@@ -21,9 +21,10 @@ object Marshallers extends JsonSupport with LazyLogging {
 
   type Marshaller = (DtState, String, DtPath) => Future[Option[String]]
 
-  def pathedFmt(s: DtState,
+  private def fmt(s: DtState,
                 t: String,
-                dtp: DtPath): Future[Option[String]] = {
+                dtp: DtPath,
+                dottedName: Boolean = false): Future[Option[String]] = {
     val f = dtDirectory ask t
     f.map((r: Any) => {
       r match {
@@ -33,9 +34,15 @@ object Marshallers extends JsonSupport with LazyLogging {
             s.state
               .map(pair => {
                 val (nameIdx, origTelem) = pair
-                val namePath: String = s"$dtp/${names(nameIdx)}"
-                val nameDots: String = namePath.replace('/', '.')
-                val lookedUpName: String = nameDots.substring(11)
+                val lookedUpName: String = {
+                  if (dottedName) {
+                    val namePath: String = s"$dtp/${names(nameIdx)}"
+                    val nameDots: String = namePath.replace('/', '.')
+                    nameDots.substring(11)
+                  } else {
+                    names(nameIdx)
+                  }
+                }
                 val origValue = origTelem.value
                 val origDatetime = origTelem.datetime
                 NamedTelemetry(lookedUpName, origValue, origDatetime)
@@ -47,29 +54,11 @@ object Marshallers extends JsonSupport with LazyLogging {
     })
   }
 
-  def namedFmt(s: DtState,
-               t: String,
-               dtp: DtPath): Future[Option[String]] = {
-    val f = dtDirectory ask t
-    f.map((r: Any) => {
-      r match {
-        case Some(dt: DtType) =>
-          val names: List[String] = dt.props.getOrElse(Set()).toList
-          Some(
-            s.state
-              .map(i => {
-                val (nameIdx, origTelem) = i
-                val lookedUpName = names(nameIdx)
-                val origValue = origTelem.value
-                val origDatetime = origTelem.datetime
-                NamedTelemetry(lookedUpName, origValue, origDatetime)
-              })
-              .toJson
-              .prettyPrint)
-        case _ => None
-      }
-    })
-  }
+  def namedFmt(s: DtState, t: String, dtp: DtPath): Future[Option[String]] =
+    fmt(s, t, dtp)
+
+  def pathedFmt(s: DtState, t: String, dtp: DtPath): Future[Option[String]] =
+    fmt(s, t, dtp, dottedName = true)
 
   def indexedFmt(s: DtState, t: String, dtp: DtPath): Future[Option[String]] =
     Future {
