83c6f32def24f8e45c06ef64b6671be52787227c
Ed Sweeney <ed@onextent.com>
2020-12-19 17:43:29 -0800

    cleanup

--- src/main/scala/somind/dtlab/operators/ApplyBuiltInOperator.scala
+++ src/main/scala/somind/dtlab/operators/ApplyBuiltInOperator.scala
@@ -9,16 +9,11 @@ object ApplyBuiltInOperator extends DtOperatorImpl with LazyLogging {
                      dtState: DtState,
                      op: Operator,
                      updateFun: Telemetry => Unit): Unit = {
-    try {
-      op.implementation match {
-        case "count" =>
-          Count(telemetry, dtState, op, updateFun)
-        case op =>
-          logger.warn(s"can not find built-in operator '$op'")
-      }
-    } catch {
-      case e: Exception =>
-        logger.error(s"can not apply operators: $e", e)
+    op.implementation match {
+      case "count" =>
+        Count(telemetry, dtState, op, updateFun)
+      case op =>
+        logger.warn(s"can not find built-in operator '$op'")
     }
   }
 
